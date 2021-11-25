2aa56f53ded19247f3cfe2d529afc3c245e2bf7e
kronosGR <geo.elgeo@gmail.com>
2021-11-25 08:48:02 +0100

    added sort to the comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -70,7 +70,7 @@
             Submit
           </button>
         </vee-form>
-        <select
+        <select v-model="sort"
           class="
             block
             mt-4
@@ -94,7 +94,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -125,10 +125,19 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! ',
       comments: [],
+      sort: '1',
     };
   },
   computed: {
     ...mapState(['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -157,6 +166,8 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
