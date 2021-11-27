8bc659b46dd3a36018bb5af65bf317389ad9fe0f
Georgios Katsanakis <geo.egleo@gmail.com>
2021-11-27 08:15:18 +0100

    added hash to links

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -14,7 +14,7 @@
   >
     <div>
       <router-link
-        :to="{name:'song', params: {id: song.docID}}"
+        :to="{ name: 'song', params: { id: song.docID } }"
         class="font-bold block text-gray-600"
         >{{ song.modified_name }}</router-link
       >
@@ -22,10 +22,16 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
+      <router-link
+        custom
+        :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </router-link>
     </div>
   </li>
 </template>
