212d3ea20e38e6cda9fbc81a3f624ecf42a81423
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-11-28 07:31:14 +0100

    added icon directive

---
+++ src/directives/icon.js
@@ -0,0 +1,6 @@
+export default {
+  beforeMount(el) {
+    // eslint-disable-next-line no-param-reassign
+    el.innerHTML += '<i class="fa fa-headphones-alt float-right text-green-400 text-2xl"></i>';
+  },
+};
