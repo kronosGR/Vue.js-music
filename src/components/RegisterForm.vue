26ce80dd9f2379fa727285721877c0d3ed60164a
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-11-29 07:26:12 +0100

    Added HTML translation with component interpolatio

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -154,7 +154,9 @@
         class="w-4 h-4 float-left -ml-6 mt-1 rounded"
         value="1"
       />
-      <label class="inline-block">Accept terms of service</label>
+      <i18n-t class="inline-block" keypath="register.accept" tag="label"
+        ><a href="#">{{ $t('register.TOS') }}</a></i18n-t
+      >
     </div>
     <ErrorMessage class="text-red-600" name="tos" />
     <button
@@ -189,15 +191,15 @@ export default {
         password: 'required|min:3|max:100',
         confirm_password: 'passwords_mismatch:@password',
         country: 'required|country_excluded:Antarctica',
-        tos: 'tos'
+        tos: 'tos',
       },
       userData: {
-        country: 'USA'
+        country: 'USA',
       },
       reg_in_submission: false,
       reg_show_alert: false,
       reg_alert_variant: 'bg-blue-500',
-      reg_alert_msg: 'Please wait! Your account is being created'
+      reg_alert_msg: 'Please wait! Your account is being created',
     };
   },
   methods: {
@@ -219,7 +221,7 @@ export default {
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
       window.location.reload();
-    }
-  }
+    },
+  },
 };
 </script>
