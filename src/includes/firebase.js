bdcbb5ef2b0c3dd15101ecab270deb96b59eda2c
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-11-30 07:38:56 +0100

    enabled db persistence

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: 'music-168bc',
   storageBucket: 'music-168bc.appspot.com',
   messagingSenderId: '720398875978',
-  appId: '1:720398875978:web:341f2d9b15b34ccb2dd5b3'
+  appId: '1:720398875978:web:341f2d9b15b34ccb2dd5b3',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,10 +18,12 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
+db.enablePersistence().catch((error) => {
+  console.log('Firebase persistence error', error);
+});
+
 const usersCollection = db.collection('users');
 const songsCollection = db.collection('songs');
 const commentsCollection = db.collection('comments');
 
-export {
-  auth, db, usersCollection, songsCollection, commentsCollection, storage
-};
+export { auth, db, usersCollection, songsCollection, commentsCollection, storage };
