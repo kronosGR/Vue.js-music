70ccfd9774041215f5e6f0f385e1ea6e365d32c8
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-11-30 20:27:57 +0100

    adde d dynamic module registration

---
+++ src/store/modules/index.js
@@ -0,0 +1,17 @@
+import { camelCase } from 'lodash';
+
+const requireModule = require.context('.', false, /\.js$/);
+
+const modules = {};
+
+requireModule.keys().forEach((filename) => {
+  if (filename === './index.js') {
+    return;
+  }
+
+  const moduleConfig = requireModule(filename);
+  const moduleName = camelCase(filename.replace(/(\.\/|.js)/g, ''));
+  modules[moduleName] = moduleConfig.default || moduleConfig;
+});
+
+export default modules;
