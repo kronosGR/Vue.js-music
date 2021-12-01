b08b96fb2dfbb36b5d446a49b57801037b88e48e
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-01 12:50:58 +0100

    mocking firebase and methods

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -2,9 +2,15 @@ import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 import SongItem from '@/components/SongItem.vue';
 
+jest.mock('@/includes/firebase', () => {
+
+});
+
 describe('Home.vue', () => {
   test('renders list of songs', () => {
     const songs = [{}, {}, {}];
+
+    Home.methods.getSongs = () => false;
 
     const component = shallowMount(Home, {
       data() {
@@ -12,9 +18,17 @@ describe('Home.vue', () => {
           songs,
         };
       },
+      global: {
+        mocks: {
+          $t: (message) => message,
+        }
+      }
     });
 
     const items = component.findAllComponents(SongItem);
     expect(items).toHaveLength(songs.length);
+    items.forEach((wrapper, i) => {
+      expect(wrapper.props().song).toBe(songs[i]);
+    });
   });
 });
